f97ced57e983c51317619b43f30018eab7cdc68e
imWildCat <wildcat.name@gmail.com>
2018-05-11 15:42:54 +0100

    Initial commit

--- MLKitDemo/ViewController.swift
+++ MLKitDemo/ViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    lazy var vision = Vision.vision()
+    var textDetector: VisionTextDetector?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        FirebaseApp.configure()
+        
+        // Important note: you must download your own GoogleService-Info.plist first.
+        performTextRecognition()
+    }
+    
+    func performTextRecognition() {
+        textDetector = vision.textDetector()
+        let uiImage = UIImage(named: "demo_image")
+        let visionImage = VisionImage(image: uiImage!)
+        let visionMetaData = VisionImageMetadata()
+        visionMetaData.orientation = .bottomRight
+        visionImage.metadata = visionMetaData
+        
+        textDetector!.detect(in: visionImage) { (features, error) in
+            guard error == nil, let features = features, !features.isEmpty else {
+                print("Error. You should also check the console for error messages.")
+                // ...
+                return
+            }
+            
+            // Recognized and extracted text
+            print("Detected text has: \(features.count) blocks")
+            for block in features {
+                print("\"\(block.text)\"")
+            }
+            // ...
+        }
     }
 
     override func didReceiveMemoryWarning() {
